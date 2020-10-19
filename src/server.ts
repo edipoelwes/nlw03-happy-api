9a0c0b01b5bd219553209a3895ac98ee0d4f4d6e
Edipo Elwes <edipoelwes2@ufpi.edu.br>
2020-10-19 12:52:29 -0300

    instalando o typeorm e criando a primeira migraçao

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 import express from 'express'
+import './database/connection'
 
 const app = express()
 
+const usuarios = [
+  {name: 'Edipo', idade: 32},
+  {name: 'Edipo', idade: 32},
+  {name: 'Edipo', idade: 32},
+  {name: 'Edipo', idade: 32},
+]
+
 app.get('/users', (request, response) => {
-  return response.json({message: 'usuarios'})
+  return response.json(usuarios)
 })
 
 app.listen(3333)
