ca20ae796ba6f45b7d42c81183400d5c2060b01a
Edipo Elwes <edipoelwes2@ufpi.edu.br>
2020-10-20 12:11:53 -0300

    metodo show de orphanages

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import OrphanagesController from './controllers/OrphanagesController'
 const route = Router()
 
 route.get('/orphanages', OrphanagesController.index)
+route.get('/orphanages/:id', OrphanagesController.show)
 route.post('/orphanages', OrphanagesController.create)
 
 export default route
