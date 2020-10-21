761ad8a0f4760c914cdc19769bc939d9afe338d1
Edipo Elwes <edipoelwes2@ufpi.edu.br>
2020-10-21 12:34:20 -0300

    lidando com exceçoes

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import express from 'express'
 import path from 'path'
+import 'express-async-errors'
 
 import './database/connection'
 import route from './routes'
+import errorHandler from './errors/handler'
 
 const app = express()
 app.use(express.json())
 app.use(route)
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandler)
 
 app.listen(3333)
 
